1ec05231fb41d75e28d35acf5f42588948b970b6
chris <c.pintarich@gmail.com>
2020-04-02 16:08:04 +0200

    drawer

--- lib/widgets/menu/main_drawer.dart
+++ lib/widgets/menu/main_drawer.dart
@@ -1,31 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:graetzl/database/dbmock.dart';
 import './menu_elements.dart';
+import '../../mixins/device_mixins.dart';
 
-class MainDrawer extends StatelessWidget {
+class MainDrawer extends StatelessWidget with DeviceSpecificMedia {
   final List categoryItems;
+
+  void _navigate(BuildContext context) {
+    Navigator.of(context).pushNamed("/categories");
+  }
 
   MainDrawer(this.categoryItems);
 
   build(BuildContext context) {
+    final _userTile = MediaQuery.of(context).padding.top + 5;
+    final _menuTile = MediaQuery.of(context).size.height;
+
+    print(_userTile);
+    print(_menuTile);
+
     return Drawer(
+      elevation: 5,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
+            padding: EdgeInsets.only(top: _userTile),
             width: double.infinity,
-            child: Text("USER"),
+            color: Theme.of(context).primaryColor,
+            child: ListTile(
+              title: Text("Olaf the Gianslayer"),
+              trailing: IconButton(
+                icon: Icon(Icons.settings),
+                onPressed: () {},
+              ),
+              leading: CircleAvatar(
+                backgroundColor: Theme.of(context).accentColor,
+                foregroundColor: Colors.white,
+                child: Icon(Icons.account_circle),
+              ),
+            ),
           ),
+          Divider(),
           Container(
             child: ListView.builder(
               itemCount: MAIN_MENU.length,
               itemBuilder: (context, index) => ListTile(
-                title: Text(
-                  MAIN_MENU[index]['title'],
-                ),
+                title: Text(MAIN_MENU[index]['title']),
+                onTap: () {
+                  _navigate(context);
+                },
               ),
             ),
-            height: 200.0,
+            // @TODO: Make this responsive
+            height: 400.0,
           ),
         ],
       ),
